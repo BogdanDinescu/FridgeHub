5ad111c5530d5d82fe8f995f59b877cec7a9d1fa
Alecsandu <alecsandu678@gmail.com>
2021-03-17 17:32:16 +0200

    Added dependencies.h
    All the includes are in here

---
+++ src/Dependencies.h
@@ -0,0 +1,23 @@
+#include <algorithm>
+#include <pistache/net.h>
+#include <pistache/http.h>
+#include <pistache/peer.h>
+#include <pistache/http_headers.h>
+#include <pistache/cookie.h>
+#include <pistache/router.h>
+#include <pistache/endpoint.h>
+#include <pistache/common.h>
+#include <signal.h>
+
+// General advice: pay atetntion to the namespaces that you use in various contexts. Could prevent headaches.
+
+// This is just a helper function to preety-print the Cookies that one of the enpoints shall receive.
+void printCookies(const Pistache::Http::Request& req) {
+    auto cookies = req.cookies();
+    std::cout << "Cookies: [" << std::endl;
+    const std::string indent(4, ' ');
+    for (const auto& c: cookies) {
+        std::cout << indent << c.name << " = " << c.value << std::endl;
+    }
+    std::cout << "]" << std::endl;
+}
